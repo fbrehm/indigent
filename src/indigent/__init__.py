608056ab29e9db5c0d51b7a1f8fd4ddacc333d8f
Frank Brehm <frank@brehm-online.com>
2025-06-16 10:21:31 +0200

    Adding setup.cfg

---
+++ src/indigent/__init__.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+@summary: Python module to render text, tables, progress bars and more to the terminal.
+
+@author: Frank Brehm
+@contact: frank@brehm-online.com
+@copyright: © 2025 by Frank Brehm, Berlin
+"""
+
+from typing import Text
+
+__author__: Text = 'Frank Brehm <frank@brehm-online.com>'
+__copyright__: Text = '(C) 2025 by Frank Brehm, Berlin'
+__contact__: Text = 'frank@brehm-online.com'
+__version__: Text = '0.1.0'
+
+
+# =============================================================================
+# vim: fileencoding=utf-8 filetype=python ts=4 et list
